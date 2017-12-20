4daef3049cdf3bc95dddf0b055419eab3ff2351c
britair <britair_ira@mail.ru>
2017-12-20 13:26:49 +0600

    mogo

--- Mogo/js/main.js
+++ Mogo/js/main.js
@@ -11,6 +11,24 @@ $(function () {
 	});
 });
 
+/*Кнопка скроллинга вверх*/
+$(document).ready(function(){   
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 0) {
+            $('#scroller').fadeIn();
+        } else {
+            $('#scroller').fadeOut();
+        }
+    });
+    $('#scroller').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 400);
+        return false;
+    });
+});
+
+
 /*Слайдер в Шапке*/
 $(document).ready(function () {
     'use strict';
